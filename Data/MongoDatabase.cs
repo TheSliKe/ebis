c18349421e0ebf8ad2a5c3683da82e546d75ca88
julien <juju.dupale@gmail.com>
2022-04-11 14:47:30 +0200

    Ajoutez des fichiers projet.

---
+++ Data/MongoDatabase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Data
+{
+    public class MongoDatabase
+    {
+
+        private MongoClient mongoClient;
+        private IMongoDatabase db;
+
+        public MongoDatabase()
+        {
+            mongoClient = new MongoClient("mongodb://localhost:27017/");
+            db = mongoClient.GetDatabase("ebis");
+        }
+
+        public List<BsonDocument> recupererListBorne()
+        {
+            var borneCollection = db.GetCollection<BsonDocument>("borne");
+
+            return borneCollection.Find(new BsonDocument()).ToList();
+
+        }
+
+    }
+}
---
+++ ebis/ListeBorne.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ebis
+{
+    /// <summary>
+    /// Logique d'interaction pour ListeBorne.xaml
+    /// </summary>
+    public partial class ListeBorne : Window
+    {
+        public ListeBorne()
+        {
+            InitializeComponent();
+            MongoDatabase mongoDatabase = new MongoDatabase();
+
+
+            mongoDatabase.recupererListBorne().ForEach(item => borneList.Items.Add(item.GetElement));
+        }
+
+    }
+}
