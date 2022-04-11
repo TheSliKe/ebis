6ba03099a4b18e3167df357981fa83e3319b2fd1
julien <juju.dupale@gmail.com>
2022-04-11 22:18:24 +0200

    refacto merging

--- ebis/ListeBorne.xaml.cs
+++ ebis/ListeBorne.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Data;
 using Microsoft.Maps.MapControl.WPF;
+using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,30 +28,22 @@ namespace Ebis
         public ListeBorne()
         {
             InitializeComponent();
-            MongoDatabase mongoDatabase = new MongoDatabase();
+            mongoDatabase = new MongoDatabase();
+
+            List<BsonDocument> listeBorne = mongoDatabase.recupererListBorne();
             
-            mongoDatabase.recupererListBorne().ForEach(item => {
-                borneList.Items.Add(item);
+            listeBorne.ForEach(item => {
+                borneList.Items.Add(item["station"]["adresseRue"].AsString);
+
                 double latitude = Convert.ToDouble(item["station"]["latitude"].AsString, System.Globalization.CultureInfo.InvariantCulture);
                 double longitude = Convert.ToDouble(item["station"]["longitude"].AsString, System.Globalization.CultureInfo.InvariantCulture);
                 Pushpin pin = new();
                 pin.Location = new Location(latitude, longitude);
                 borneMap.Children.Add(pin);
             });
-        }
-            mongoDatabase = new MongoDatabase();
 
-            mongoDatabase.recupererListBorne().ForEach(item => borneList.Items.Add(item["station"]["adresseRue"].AsString));
         }
 
-        private void borneList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            //TODO Au click: zoom sur la map
-            //Location loc = (latitude, longitude);
-            //borneMap.SetView(loc, 12);
-        }
-    }
-}
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
@@ -58,12 +51,20 @@ namespace Ebis
             {
                 borneList.Items.Clear();
                 mongoDatabase.recupererListBorne(recherche.Text).ForEach(item => borneList.Items.Add(item["station"]["adresseRue"].AsString));
-            } else
+            }
+            else
             {
                 borneList.Items.Clear();
                 mongoDatabase.recupererListBorne().ForEach(item => borneList.Items.Add(item["station"]["adresseRue"].AsString));
             }
-           
+
+        }
+
+        private void borneList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //TODO Au click: zoom sur la map
+            //Location loc = (latitude, longitude);
+            //borneMap.SetView(loc, 12);
         }
     }
 }
