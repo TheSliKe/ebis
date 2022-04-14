150f3c218e7b344df0f6206ea3b803556c03c9b0
Quentin <59879188+quentinDelaporte@users.noreply.github.com>
2022-04-14 09:57:44 +0200

    graphique bonus 1

---
+++ ebis/Tabs/Charts/partIncident.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Data;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace Ebis.Tabs.Charts
+{
+    public partial class PartIncident : UserControl
+    {
+        private MongoDatabase mongoDatabase;
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
+
+        public PartIncident()
+        {
+            InitializeComponent();
+            PointLabel = chartPoint =>
+                string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+
+            DataContext = this;
+        }
+        public Func<ChartPoint, string> PointLabel { get; set; }
+        private void Chart_OnDataClick(object sender, ChartPoint chartpoint)
+        {
+            var chart = (PieChart)chartpoint.ChartView;
+
+            //clear selected slice.
+            foreach (PieSeries series in chart.Series)
+                series.PushOut = 0;
+
+            var selectedSeries = (PieSeries)chartpoint.SeriesView;
+            selectedSeries.PushOut = 8;
+        }
+
+        private void initGraphMoyenneFonctionnement()
+        {
+            Dictionary<string, int> avgMap = mongoDatabase.StatPartNiveauIncident();
+
+
+
+        }
+    }
+}
